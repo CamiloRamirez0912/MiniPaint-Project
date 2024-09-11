025e31039bb19e65cf2f31556fcfe28f744c1dd0
Camilo Ramirez <camiestebanramirezsalas@gmail.com>
2024-09-11 00:31:23 -0500

    Added buttom panel, fonts, sizes and font colors

--- src/views/MenuOptions.java
+++ src/views/MenuOptions.java
@@ -1,18 +1,24 @@
 package views;
 
 import java.awt.*;
-
 import javax.swing.*;
 
-public class MenuOptions extends JMenuBar{
+public class MenuOptions extends JMenuBar {
     private JMenu menuSettings;
     private JMenu menuTools;
     private JMenu menuAbout;
-    
-    public MenuOptions(){
+    private Font customFont;
+
+    public MenuOptions() {
+        customFont = new Font("Arial", Font.BOLD, 16);
+
         menuSettings = new JMenu("Configuración");
+        menuSettings.setFont(customFont);
         menuTools = new JMenu("Herramientas");
+        menuTools.setFont(customFont);
         menuAbout = new JMenu("Acerca de");
+        menuAbout.setFont(customFont);
+
         this.setPreferredSize(new Dimension(this.getWidth(), 50));
         this.setBorder(BorderFactory.createEmptyBorder());
         this.add(menuTools);
@@ -21,25 +27,32 @@ public class MenuOptions extends JMenuBar{
         setMenuSettings();
     }
 
-    private void setMenuSettings(){
+    private void setMenuSettings() {
         JMenuItem color = new JMenuItem("Color");
         JMenuItem clean = new JMenuItem("Limpiar");
         JMenuItem exit = new JMenuItem("Salir");
+
+        color.setFont(customFont);
+        clean.setFont(customFont);
+        exit.setFont(customFont);
+
         menuSettings.add(color);
         menuSettings.add(clean);
         menuSettings.add(exit);
         this.add(menuSettings);
     }
 
-    private void setMenuTools(){
-        JMenuItem color = new JMenuItem("Linea");
-        JMenuItem clean = new JMenuItem("Circulo");
-        JMenuItem exit = new JMenuItem("Rectangulo");
-        menuSettings.add(color);
-        menuSettings.add(clean);
-        menuSettings.add(exit);
-        this.add(menuSettings);
+    private void setMenuTools() {
+        JMenuItem linea = new JMenuItem("Linea");
+        JMenuItem circulo = new JMenuItem("Circulo");
+        JMenuItem rectangulo = new JMenuItem("Rectangulo");
+
+        linea.setFont(customFont);
+        circulo.setFont(customFont);
+        rectangulo.setFont(customFont);
+
+        menuTools.add(linea);
+        menuTools.add(circulo);
+        menuTools.add(rectangulo);
     }
-
-
 }
--- src/views/Window.java
+++ src/views/Window.java
@@ -4,22 +4,35 @@ import java.awt.*;
 
 import javax.swing.JFrame;
 
-public class Window extends JFrame{
-
-    private MenuOptions menuOptions ;
-    DrawingBoard drawingBoard = new DrawingBoard();
+public class Window extends JFrame {
+    private MenuOptions menuOptions;
+    private DrawingBoard drawingBoard;
+    private BottomPanel bottomPanel;
     private String actualTool;
+    private Font customFont;
 
-    
-    public Window(){
+    public Window() {
+        customFont = new Font("Arial", Font.BOLD, 16);
+
         setExtendedState(JFrame.MAXIMIZED_BOTH);
-        getContentPane().setBackground(Color.CYAN);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setBackground(Color.WHITE);
+
         menuOptions = new MenuOptions();
+        drawingBoard = new DrawingBoard();
+        bottomPanel = new BottomPanel();
+
         setJMenuBar(menuOptions);
-        add(drawingBoard);
+
+        setLayout(new BorderLayout());
+        add(drawingBoard, BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.SOUTH);
+
+        bottomPanel.updateColor(Color.BLACK);
+        bottomPanel.updateElementCount(0);
     }
 
-    private void setDrawingBoard(){
-        
-    }
+    /*private void setDrawingBoard() {
+
+    }*/
 }
